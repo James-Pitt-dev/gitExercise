2af290b2a6d71922386ef3d66483751fa88a85b7
James Pitt <39842510+James-Pitt-dev@users.noreply.github.com>
2024-04-05 15:03:39 -0700

    Beginning frontEnd form

---
+++ app.js
@@ -0,0 +1,8 @@
+const addition = (a, b) => {
+    if (typeof(a) !== 'number' || typeof(b) !== 'number'){
+        throw new Error('Input must be a number')
+    }
+    return a + b;
+}
+
+module.exports = {addition}
